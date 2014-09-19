8e2184c8f1404a17a09c87834eb074e57e2ac6b1
Phil Ruffwind <rf@rufflewind.com>
2014-09-19 16:50:18 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,88 +1,69 @@
-import os, shutil
+import os
 
 '''A file object to `/dev/null`.'''
 DEV_NULL = open(os.devnull, "r+")
 
 def read_file(filename):
-    '''Reads a file and returns its contents as a string.'''
+    '''Read a file and return its contents as a string.'''
     with open(filename) as f:
         return f.read()
 
-def write_file(filename, contents, only_if_different=False):
-    '''Writes a file with the given string as contents.'''
-    if only_if_different:
+def write_file(filename, contents, only_if_different=False, safe=True):
+    '''Write a file with the given string as contents.
+
+    If `safe` is `True`, the operation is done "as atomically as possible": if
+    the filesystem supports atomic rename then it will be performed
+    atomically; if the filesystem does not, it will, in the worst case, leave
+    behind a temporary file with a similar name that contains the new
+    contents.'''
+
+    # encode `contents` into bytes, but only on Python 3
+    contents_encode = getattr(contents, "encode", None)
+    if contents_encode and getattr(contents, "decode", None):
+        import locale
+        contents = contents_encode(locale.getpreferredencoding(False))
+
+    # write to temporary file first
+    import os, tempfile
+    name, ext = os.path.splitext(os.path.basename(filename))
+    with tempfile.NamedTemporaryFile(dir=os.path.dirname(filename),
+                                     prefix=name + ".",
+                                     suffix=ext + ".tmp",
+                                     delete=False) as tmp:
+        tmp_fn = tmp.name
         try:
-            old_contents = read_file(filename)
-        except Exception:
-            old_contents = None
-        if contents == old_contents:
-            return
-    with open(filename, "w") as f:
-        f.write(contents)
-
-class TmpFile(object):
-    '''A context object representing a temporary file, which is deleted upon
-    exiting the context.'''
-
-    def __init__(self, contents, suffix=""):
-        '''Represents a temporary file with the given string as contents.'''
-        self.contents = contents
-        self.suffix   = suffix
-
-    def __enter__(self):
-        '''Create the temporary file and return this object: the `fd` member
-        contains the file descriptor and `filename` member contains the path
-        to the temporary file.'''
-        import tempfile
-        self.fd, self.filename = tempfile.mkstemp(suffix=self.suffix)
-        with os.fdopen(self.fd, "w") as f:
-            f.write(self.contents)
-        return self
-
-    def __exit__(self, type, value, traceback):
-        '''Remove the temporary file.'''
-        try:
-            os.remove(self.filename)
+            tmp.file.write(contents)
         except:
-            pass
+            os.remove(tmp_fn)
+            raise
 
-class TmpDir(object):
-    '''A context representing a temporary directory, which is deleted upon
-    exiting the context.'''
-
-    def __init__(self, *args, **kwargs):
-        '''Represents a temporary directory.  The parameters of this function is
-        identical to that of `tempfile.mkdtemp`.'''
-        self.args   = args
-        self.kwargs = kwargs
-
-    def __enter__(self):
-        '''Create a temporary directory and return its path.'''
-        import tempfile
-        self.dirname = tempfile.mkdtemp(*self.args, **self.kwargs)
-        return self.dirname
-
-    def __exit__(self, type, value, traceback):
-        '''Remove the temporary directory.'''
-        shutil.rmtree(self.dirname, ignore_errors=True)
+    # overwrite target file with temporary file
+    try:
+        os.rename(tmp_fn, filename)
+    except OSError:
+        import shutil
+        os.remove(filename)
+        os.rename(tmp_fn, filename)
 
 class WorkDir(object):
     '''A context for changing to a different working directory.  The original
     working directory is restored upon exiting the context.'''
 
     def __init__(self, path):
-        '''Represents a working directory.'''
+        '''Initialize a context for changing the working directory.'''
         self.path = path
 
     def __enter__(self):
         '''Change the working directory and return the path to the previous
         working directory.'''
+        import os
         self.prevdir = os.getcwd()
         os.chdir(self.path)
         return self.prevdir
 
     def __exit__(self, type, value, traceback):
         '''Restore the the temporary directory.'''
+        import os
         os.chdir(self.prevdir)
 
 class FileLock(object):
@@ -90,12 +71,12 @@ class FileLock(object):
     exiting the context.  (Unix only.)'''
 
     def __init__(self, path, block=True):
-        '''Represents a file lock.'''
+        '''Initialize a context for holding an exclusive file lock.'''
         self.path  = path
         self.block = block
 
     def __enter__(self):
-        '''Grabs the file lock.  Returns nothing.  If `block` was set to true,
+        '''Acquire the lock.  Returns nothing.  If `block` was set to true,
         then an `IOError` is raised if the file is already locked.'''
         import fcntl
         flags = fcntl.LOCK_EX
@@ -109,5 +90,5 @@ class FileLock(object):
             raise
 
     def __exit__(self, type, value, traceback):
-        '''Release the file lock.'''
+        '''Release the lock.'''
         self.lockfile.close()
