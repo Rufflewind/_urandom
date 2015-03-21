0f409f8a01d81c624b10e92b68dfb4f5500445ff
Phil Ruffwind <rf@rufflewind.com>
2015-03-21 09:43:00 -0400

    update script for installing GHC

--- get-ghc.sh
+++ get-ghc.sh
@@ -1,26 +1,69 @@
 #!/bin/sh
-PREFIX="$HOME"
-ARCH=x86_64
+# script for installing GHC on Linux
 
-GHC_VER=7.8.2
-GHC_DIST=unknown-linux-centos65
-# note: unknown-linux-centos65 requires GMP4 (libgmp.so.3)
+prefix=/usr/local
+arch=x86_64
 
-CABAL_VER=1.20.0.1
-CABAL_DIST=unknown-linux
+ghc_ver=7.8.4
+#ghc_dist=unknown-linux-centos65 # requires GMP4 (libgmp.so.3)
+ghc_dist=unknown-linux-deb7 # requires GMP5 (libgmp.so.10)
 
-U=http://haskell.org/ghc/dist
-U="$U"/"$GHC_VER"/ghc-"$GHC_VER"-"$ARCH"-"$GHC_DIST".tar.xz
-curl -L "$U" | tar xJf -
+cabal_ver=1.22.0.0
+cabal_dist=unknown-linux
+
+# ----------------------------------------------------------------------------
+set -e
+
+# download a file from the given URL
+#
+# inputs:
+#   - 1:            URL
+#
+# output:
+#   - /dev/stdout:  data of downloaded file
+#
+download_tool=
+download() {
+    if [ $# -ne 1 ]
+    then
+        echo >&2 "download: expected one argument"
+        return 1
+    fi
+
+    # figure out which tool we should use to download files
+    [ "$download_tool" ] || {
+        if command >/dev/null 2>&1 -v curl
+        then
+            download_tool=curl
+        elif command >/dev/null 2>&1 -v wget
+        then
+            download_tool=wget
+        else
+            download_tool=none
+        fi
+    }
+
+    # call the download tool
+    case $download_tool in
+        curl) curl -fsLS    -- "$1";;
+        wget) wget -nv -O - -- "$1";;
+        *)    echo >&2 "download: either `curl` or `wget` must be installed"
+              return 1;;
+    esac
+}
+
+url=http://haskell.org/ghc/dist
+url=$url/$ghc_ver/ghc-$ghc_ver-$arch-$ghc_dist.tar.xz
+download "$url" | tar xJf -
 (
-    cd ghc-7.8.2
-    ./configure --prefix="$PREFIX"
+    cd "ghc-$ghc_ver"
+    ./configure --prefix="$prefix"
     make install
 )
 
-U=http://haskell.org/cabal/release
-U="$U"/cabal-install-"$CABAL_VER"/cabal-"$ARCH"-"$CABAL_DIST".tar.gz
+url=http://haskell.org/cabal/release
+url=$url/cabal-install-$cabal_ver/cabal-$arch-$cabal_dist.tar.gz
 (
-    cd "$PREFIX"/bin
-    curl -L "$U" | tar xzf -
+    cd "$prefix/bin"
+    download "$url" | tar xzf -
 )
--- shell-utility.sh
+++ shell-utility.sh
@@ -1,3 +1,41 @@
+# download a file from the given URL
+#
+# inputs:
+#   - 1:            URL
+#
+# output:
+#   - /dev/stdout:  data of downloaded file
+#
+download_tool=
+download() {
+    if [ $# -ne 1 ]
+    then
+        echo >&2 "download: expected one argument"
+        return 1
+    fi
+
+    # figure out which tool we should use to download files
+    [ "$download_tool" ] || {
+        if command >/dev/null 2>&1 -v curl
+        then
+            download_tool=curl
+        elif command >/dev/null 2>&1 -v wget
+        then
+            download_tool=wget
+        else
+            download_tool=none
+        fi
+    }
+
+    # call the download tool
+    case $download_tool in
+        curl) curl -fsLS    -- "$1";;
+        wget) wget -nv -O - -- "$1";;
+        *)    echo >&2 "download: either `curl` or `wget` must be installed"
+              return 1;;
+    esac
+}
+
 # perform a one-sided exponential search to find the
 # largest value at which the trial function succeeds
 #
