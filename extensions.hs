223e2959d127e1042a4dc1d4ab5b76520841a1e6
Phil Ruffwind <rf@rufflewind.com>
2015-09-12 11:13:22 -0400

    extensions.hs

---
+++ extensions.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE BangPatterns
+           , DeriveDataTypeable
+           , EmptyDataDecls
+           , FlexibleContexts
+           , FlexibleInstances
+           , ForeignFunctionInterface
+           , FunctionalDependencies
+           , GADTs
+           , Generics
+           , RankNTypes
+           , ScopedTypeVariables
+           , TypeFamilies
+           , TypeOperators
+           , ViewPatterns #-}
