3e75b68c5de0f544cab8bc4246b40792b72b50d9
Phil Ruffwind <rf@rufflewind.com>
2020-06-07 23:43:44 -0700

    Update typeutil.py

--- typeutil.py
+++ typeutil.py
@@ -4,5 +4,6 @@ T = TypeVar('T')
 
 def to(ty: Type[T], value: object) -> T:
     if not isinstance(value, ty):
-        raise TypeError(f'expected {ty!r}, got {value!r}')
+        raise TypeError(f'expected {ty.__qualname__}, got {value!r}: '
+                        f'{type(value).__qualname__}')
     return value
